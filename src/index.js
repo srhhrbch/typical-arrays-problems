93a55bc454c59e321ade1b413e160d180ae73fa3
Siarhei Horbach <srhhrbch@gmail.com>
2020-09-29 02:05:02 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    };
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max(...array);
+    };
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let average = array.reduce((a, b) => a + b) / array.length;
+        return average;
+    };
+};
